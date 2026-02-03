1e66da033528fd1de5e5be0ba791644ca9b0c851
sharma151 <sharmasaurav150503@gmail.com>
2026-02-03 14:16:25 +0545

    fix: groupchat dropdown items added

--- src/pages/chats/ChatRoomNav/index.tsx
+++ src/pages/chats/ChatRoomNav/index.tsx
@@ -3,8 +3,23 @@ import type { MenuProps } from "antd";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { MdDelete } from "react-icons/md";
 import { useActiveChat } from "@/core/hooks/common/useActiveChat";
+import { AiOutlineInfoCircle } from "react-icons/ai";
+import { FaUserEdit } from "react-icons/fa";
+import { HiUserAdd } from "react-icons/hi";
+import { GiExitDoor } from "react-icons/gi";
 const ChatRoomNav = ({ handleMenuClick }: { handleMenuClick?: () => void }) => {
   const items: MenuProps["items"] = [
+    { key: "Delete", label: "Delete chat", icon: <MdDelete size={16} /> },
+  ];
+  const GroupChatItems: MenuProps["items"] = [
+    {
+      key: "GroupInfo",
+      label: "View Group Info",
+      icon: <AiOutlineInfoCircle size={16} />,
+    },
+    { key: "Rename", label: "Rename group", icon: <FaUserEdit size={16} /> },
+    { key: "Add", label: "Add members", icon: <HiUserAdd size={16} /> },
+    { key: "Leave", label: "Leave group", icon: <GiExitDoor size={16} /> },
     { key: "Delete", label: "Delete chat", icon: <MdDelete size={16} /> },
   ];
 
@@ -26,7 +41,7 @@ const ChatRoomNav = ({ handleMenuClick }: { handleMenuClick?: () => void }) => {
       </div>
 
       <CustomDropdown
-        items={items}
+        items={activeChat?.isGroup ? GroupChatItems : items}
         triggerContent={<BsThreeDotsVertical size={18} />}
         onMenuClick={handleMenuClick}
       />
